6e0384667d01624401bf9c11b6090691e492cb48
EC2 Default User <ec2-user@ip-172-31-10-18.eu-west-2.compute.internal>
2021-07-21 09:56:25 +0000

    the first commit

---
+++ test2.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+#for ((i=1;i<=10;i++)); do
+#	echo $i
+#done
+
+#for i in {1..10}; do
+#	echo $i
+#done
+
+#i=1
+#while [ $i -le 10 ]; do
+#	echo $i
+#	((i++))
+#done
+
+echo "What is your name?"
+read name
+echo "Nice to meet you $name"
